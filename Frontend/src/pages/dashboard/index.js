d5319fb8c2780a3f981e328a89ddc53dfb11a4ac
trintrintrin <64020811@up.ac.th>
2024-05-16 14:04:47 +0700

    5/16

--- Frontend/src/pages/dashboard/index.js
+++ Frontend/src/pages/dashboard/index.js
@@ -1,22 +1,54 @@
-import "./index.css";
-import React, {useState} from 'react'
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
-
-
+import React from 'react';
+import { Calendar, Whisper, Popover, Badge } from 'rsuite';
+import 'rsuite/Calendar/styles/index.css';
+import 'pages/dashboard/index.css';
 export default function App() {
-  const [dateState, setDateState] = useState(new Date())
-  const changeDate = (e) => {
-    setDateState(e)
+  function renderCell() {
+    const list = [];
+
+    const displayList = list.slice(0, 2);
+
+    if (list.length) {
+      const moreCount = list.length - displayList.length;
+      const moreItem = (
+        <li key="more">
+          <Whisper
+            placement="top"
+            trigger="click"
+            speaker={
+              <Popover>
+                {list.map((item, index) => (
+                  <p key={index}>
+                    <b>{item.time}</b> - {item.title}
+                  </p>
+                ))}
+              </Popover>
+            }
+          >
+            <a>{moreCount} more</a>
+          </Whisper>
+        </li>
+      );
+
+      return (
+        <ul className="calendar-todo-list">
+          {displayList.map((item, index) => (
+            <li key={index}>
+              <Badge /> <b>{item.time}</b> - {item.title}
+            </li>
+          ))}
+          {moreCount ? moreItem : null}
+        </ul>
+      );
+    }
+
+    return null;
   }
   return (
-    <>
-      <Calendar 
-      value={dateState}
-      onChange={changeDate}
-      />
-    
-    </>
-  )
+    <div className="container">
+      <div className="calendar-wrapper">
+        <Calendar bordered renderCell={renderCell} />
+      </div>
+    </div>
+  );
 }
-

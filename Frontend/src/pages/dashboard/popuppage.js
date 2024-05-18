ec1a8a078aade784e31ca31b7989dac383ce225d
trintrintrin <64020811@up.ac.th>
2024-05-18 19:04:24 +0700

    18/5
    18/5

---
+++ Frontend/src/pages/dashboard/popuppage.js
@@ -0,0 +1,15 @@
+// PopupPage.js
+import React from 'react';
+
+const PopupPage = () => {
+  return (
+    <div className="popup-container">
+      <div className="popup-content">
+        <h2>Popup Page</h2>
+        <p>This is the content of the popup page.</p>
+      </div>
+    </div>
+  );
+}
+
+export default PopupPage;
